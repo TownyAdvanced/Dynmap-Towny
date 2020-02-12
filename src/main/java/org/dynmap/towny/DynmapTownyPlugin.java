5019a6bc8fcd6a695777bd6c078597b14470b329
goosius1 <goosius1@gmail.com>
2020-02-12 12:19:24 +0000

    dynamic colours - a few changes for usability

--- src/main/java/org/dynmap/towny/DynmapTownyPlugin.java
+++ src/main/java/org/dynmap/towny/DynmapTownyPlugin.java
@@ -484,18 +484,18 @@ public class DynmapTownyPlugin extends JavaPlugin {
                 Nation nation = town.getNation();
                 String nationBoard = nation.getNationBoard();
 
-                if(nationBoard.contains("map_border_style=")) {
-                    String[] styleValues = nationBoard.split("map_border_style=")[1].split(",");
-                    int strokeColor =  Integer.parseInt(styleValues[0].trim());
+                if(nationBoard.contains("mapcolor-")) {
+                    String[] styleValues = nationBoard.split("mapcolor-")[1].split(",");
+                    int nationColor =  Integer.parseInt(styleValues[0].trim());
+
+                    //Set stroke style
                     double strokeOpacity = m.getLineOpacity();
-                    int strokeWeight = Integer.parseInt(styleValues[1].trim());
-                    m.setLineStyle(strokeColor, strokeOpacity, strokeWeight);
-                }
-                if(nationBoard.contains("map_fill_style=")) {
-                    String[] styleValues = nationBoard.split("map_fill_style=")[1].split(",");
-                    int fillColor =  Integer.parseInt(styleValues[0].trim());
+                    int strokeWeight = m.getLineWeight();
+                    m.setLineStyle(nationColor, strokeOpacity, strokeWeight);
+
+                    //Set fill style
                     double fillOpacity = m.getFillOpacity();
-                    m.setFillStyle(fillColor, (int)fillOpacity);
+                    m.setFillStyle(nationColor, (int)fillOpacity);
                 }
             }
         } catch (Exception ex) {}
